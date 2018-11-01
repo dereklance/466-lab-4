80d550860c1d2225bf75494caf45209573c0f9e0
Derek Lance <derekwarnerlance@yahoo.com>
2018-11-01 16:04:00 -0700

    Completed csv parser

--- parse.py
+++ parse.py
@@ -1,8 +1,22 @@
+def indexOf(array, value):
+	for index, item in enumerate(array):
+		if int(item) == value:
+			return index
+	return -1
+
 def csv(filename):
 	with open(filename, 'r') as file:
 		lines = file.read().strip().split('\n')
-		unusedColumnIndex = lines[0].strip().split(',')
-		print(unusedColumnIndex)
+		header = lines[0].strip().split(',')
+		unusedIndex = indexOf(header, 0)
 
-		for line in lines:
+		data = []
+		for line in lines[1:]:
 			attributes = line.strip().split(',')
+
+			if unusedIndex >= 0:
+				del attributes[unusedIndex]
+			
+			data.append([float(value) for value in attributes])
+
+		return data
